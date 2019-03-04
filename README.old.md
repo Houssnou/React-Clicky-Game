3b00a29039f12383f87ce4e1c8b6d069757f0e02
Houssnou <hosni.moubarak@gmail.com>
2019-03-04 18:35:52 -0500

    Updates

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# React-Clicky-Game
-React-Clicky-Game
---
+++ precache-manifest.a857cc215d28d00db21906418566f97b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e410ef131087a5d5cfb",
+    "url": "/React-Clicky-Game/static/js/runtime~main.4e410ef1.js"
+  },
+  {
+    "revision": "126b1c65663191f055e9",
+    "url": "/React-Clicky-Game/static/js/main.126b1c65.chunk.js"
+  },
+  {
+    "revision": "9faaf100b571f1d79c3b",
+    "url": "/React-Clicky-Game/static/js/2.9faaf100.chunk.js"
+  },
+  {
+    "revision": "d56d6482911b4f09a5affb9e12f2b71d",
+    "url": "/React-Clicky-Game/index.html"
+  }
+];
